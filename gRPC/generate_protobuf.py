830ec1b5b9de297ad661bcdbc4b137838cf4ba74
Anna Vichugova <112316409+AnnaVichugova@users.noreply.github.com>
2025-04-17 18:05:18 +0700

    generate_protobuf.py

--- gRPC/generate_protobuf.py
+++ gRPC/generate_protobuf.py
@@ -1,3 +1,8 @@
 import os
 
 os.system('python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. order.proto')
+
+
+###### если в Colab
+!pip install grpcio grpcio-tools
+!python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. person.proto
